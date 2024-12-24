5280d5be8dc3036a4752cea63672ee992e477ecd
nocturne228 <206088020@qq.com>
2024-12-24 16:58:29 +0800

    PA2.2: Implement ftrace basically but with function name and log write to resolve

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -71,6 +71,10 @@ static long load_img() {
   return size;
 }
 
+static char *elf_file = NULL;
+
+void parse_elf(const char *elf_file);
+
 static int parse_args(int argc, char *argv[]) {
   const struct option table[] = {
       {"batch", no_argument, NULL, 'b'},
@@ -78,10 +82,11 @@ static int parse_args(int argc, char *argv[]) {
       {"diff", required_argument, NULL, 'd'},
       {"port", required_argument, NULL, 'p'},
       {"help", no_argument, NULL, 'h'},
+      {"elf", required_argument, NULL, 'e'},
       {0, 0, NULL, 0},
   };
   int o;
-  while ((o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ((o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
     switch (o) {
       case 'b':
         sdb_set_batch_mode();
@@ -95,6 +100,9 @@ static int parse_args(int argc, char *argv[]) {
       case 'd':
         diff_so_file = optarg;
         break;
+      case 'e': 
+        elf_file = optarg; 
+        break;
       case 1:
         img_file = optarg;
         return 0;
@@ -105,6 +113,7 @@ static int parse_args(int argc, char *argv[]) {
         printf(
             "\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=FILE           elf file to be parsed\n");
         printf("\n");
         exit(0);
     }
@@ -143,6 +152,7 @@ void init_monitor(int argc, char *argv[]) {
   init_sdb();
 
   IFDEF(CONFIG_ITRACE, init_disasm());
+  IFDEF(CONFIG_ITRACE, parse_elf(elf_file));
 
   /* Display welcome message. */
   welcome();
