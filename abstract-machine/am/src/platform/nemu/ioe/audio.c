38288a35ec4e7ce2178729a53bb8878dbc826385
nocturne228 <206088020@qq.com>
2024-12-26 23:39:50 +0800

    PA2.3: Implement Sound Card

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,19 +8,37 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+static size_t sbuf_pos = 0;
+static uint32_t sbuf_size = 0;
+
 void __am_audio_init() {
+  sbuf_pos = 0;
+  sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = sbuf_size;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+
+  outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  const uint16_t *const buf = ctl->buf.start;
+  const uint32_t len = (uint16_t *)(ctl->buf.end) - buf;
+
+  for (size_t i = 0; i < len; i++) {
+    outw(AUDIO_SBUF_ADDR + sbuf_pos, buf[i]);
+    sbuf_pos = (sbuf_pos + 2) % sbuf_size;
+  }
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -17,6 +17,8 @@
 #include <device/map.h>
 #include <SDL2/SDL.h>
 
+#define Warn printf
+
 enum {
   reg_freq,
   reg_channels,
@@ -27,10 +29,111 @@ enum {
   nr_reg
 };
 
+static bool audio_opened = false;
+
 static uint8_t *sbuf = NULL;
+static size_t sbuf_pos = 0;
+static size_t sbuf_count = 0;
 static uint32_t *audio_base = NULL;
 
+typedef struct AudioSpec_ {
+  uint32_t freq;
+  uint32_t channels;
+  uint32_t samples;
+} AudioSpec_t;
+
+static AudioSpec_t audio_spec;
+
+static void callback_play(void *user_data, uint8_t *stream, int len) {
+  memset(stream, 0, len);
+  const size_t chunk_len = (size_t)len > sbuf_count ? sbuf_count : (size_t)len;
+  if ((sbuf_pos + chunk_len) > CONFIG_SB_SIZE) {
+    const uint32_t len_rem = CONFIG_SB_SIZE - sbuf_pos;
+    SDL_MixAudio(stream, sbuf + sbuf_pos, len_rem, SDL_MIX_MAXVOLUME);
+    SDL_MixAudio(stream + len_rem, sbuf, chunk_len - len_rem, SDL_MIX_MAXVOLUME);
+  } else {
+    SDL_MixAudio(stream, sbuf + sbuf_pos, chunk_len, SDL_MIX_MAXVOLUME);
+  }
+  sbuf_pos = (sbuf_pos + chunk_len) % CONFIG_SB_SIZE;
+  sbuf_count -= chunk_len;
+}
+
+static void do_sdl_audio_init(void) {
+  sbuf_pos = 0;
+  sbuf_count = 0;
+
+  SDL_AudioSpec want = {
+      .freq = audio_spec.freq,
+      .channels = audio_spec.channels,
+      .samples = audio_spec.samples,
+      .format = AUDIO_S16SYS,
+      .userdata = NULL,
+      .callback = callback_play,
+  };
+
+  if (SDL_InitSubSystem(SDL_INIT_AUDIO)) {
+    Warn("%s", SDL_GetError());
+  }
+  if (SDL_OpenAudio(&want, NULL)) {
+    Warn("%s", SDL_GetError());
+  }
+  SDL_PauseAudio(0);
+
+  audio_opened = true;
+}
+
+static void do_sdl_audio_shutdown(void) {
+  SDL_PauseAudio(1);
+  SDL_CloseAudio();
+  SDL_QuitSubSystem(SDL_INIT_AUDIO);
+  audio_opened = false;
+}
+
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  assert(len == 4);
+  switch (offset / sizeof(uint32_t)) {
+    case reg_freq:
+      if (is_write) {
+        audio_spec.freq = audio_base[reg_freq];
+      }
+      break;
+    case reg_channels:
+      if (is_write) {
+        audio_spec.channels = audio_base[reg_channels];
+      }
+      break;
+    case reg_samples:
+      if (is_write) {
+        audio_spec.samples = audio_base[reg_samples];
+      }
+      break;
+    case reg_sbuf_size:
+      if (!is_write) {
+        audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+      }
+      break;
+    case reg_init:
+      if (is_write && audio_base[reg_init]) {
+        if (audio_opened) {
+          do_sdl_audio_shutdown();
+        }
+        do_sdl_audio_init();
+        audio_base[reg_init] = 0;
+      }
+      break;
+    case reg_count:
+      if (!is_write) {
+        audio_base[reg_count] = (uint32_t)sbuf_count;
+      }
+      break;
+    default: panic("do not support offset = %d", offset);
+  }
+}
+
+static void audio_sbuf_handler(uint32_t offset, int len, bool is_write) {
+  if (likely(is_write)) {
+    sbuf_count += len;
+  }
 }
 
 void init_audio() {
@@ -43,5 +146,5 @@ void init_audio() {
 #endif
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
-  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, audio_sbuf_handler);
 }
