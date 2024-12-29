9801a32a4ff6bc53987a6dcb05706352d5dc81e5
Nocturne2282282 <206088020@qq.com>
2024-12-29 22:27:24 +0800

    PA2.3: Reformat gpu.c

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,18 +3,14 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void __am_gpu_init() {
-}
+void __am_gpu_init() {}
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t screen_wh = inl(VGACTL_ADDR);
-  uint32_t h = screen_wh & 0xffff;
-  uint32_t w = screen_wh >> 16;
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = w, .height = h,
-    .vmemsz = 0
-  };
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = inw(VGACTL_ADDR + 2),
+                           .height = inw(VGACTL_ADDR),
+                           .vmemsz = 0};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
@@ -23,9 +19,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *pixels = ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
-  for (int i = y; i < y+h; i++) {
-    for (int j = x; j < x+w; j++) {
-      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+  for (int i = y; i < y + h; i++) {
+    for (int j = x; j < x + w; j++) {
+      fb[screen_w * i + j] = pixels[w * (i - y) + (j - x)];
     }
   }
   if (ctl->sync) {
@@ -33,6 +29,4 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
-}
+void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
